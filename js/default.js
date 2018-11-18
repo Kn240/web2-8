c99c12fd0763a7e3745335db0d5e3be0364fe618
Kn240 <35630007+Kn240@users.noreply.github.com>
2018-11-18 23:58:21 +0900

    Update default.js

--- js/default.js
+++ js/default.js
@@ -1,19 +1,18 @@
 var separate_time = function(time){
-  var sec   = time.getSeconds();
-  var min   = time.getMinutes();
-  var hours = time.getHours();
-  var days  = time.getDate();
-  var month = time.getMonth();
-  var year  = time.getFullYear();
-  return [sec,min,hours,days,month,year];
+  var sec   = Math.floor((time / 1000) % 60);
+  var min = Math.floor((time / 1000 / 60) % 60);
+  var hours  = Math.floor((time / 1000 / 60 / 60) % 24);
+  var days = Math.floor(time / 1000 / 60 / 60 / 24);
+  return [sec,min,hours,days];
 }
 
 var now = new Date();
-var counter = separate_time(now);
+var target = new Date(2020,7,24,0,0,0,0);
+var diff = target.getTime() - now.getTime();
+var counter = separate_time(diff);
 document.getElementById("countdown").textContent =
-  conuter[5] + "年" +
-  conuter[4] + "月" +
+  "東京オリンピックまであと　”　+
   conuter[3] + "日" +
-  conuter[2] + "時" +
+  conuter[2] + "時間" +
   conuter[1] + "分" +
   conuter[0] + "秒";
