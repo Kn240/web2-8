b116a78104d406072ab614802301776b57618724
Kn240 <35630007+Kn240@users.noreply.github.com>
2018-11-18 23:46:06 +0900

    Update default.js

--- js/default.js
+++ js/default.js
@@ -1,3 +1,19 @@
-var li = document.createElement("li");
-li.textContent = "アイテム";
-document.getElementById("list").appendChild(li);
+var separate_time = function(time){
+  var sec   = time.getSeconds();
+  var min   = time.getMinutes();
+  var hours = time.getHours();
+  var days  = time.getDate();
+  var month = time.getMonth();
+  var year  = time.getFullYear();
+  return [sec,min,hours,days,month,year];
+}
+
+var now = new Date();
+var counter = separate_time(now);
+document.getElementById("countdown").textContent =
+  conuter[5] + "年" +
+  conuter[4] + "月" +
+  conuter[3] + "日" +
+  conuter[2] + "時" +
+  conuter[1] + "分" +
+  conuter[0] + "秒";
